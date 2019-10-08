9169486a78c962457dcc2d8f7188a4916faec4a7
aliciawongg <w.alicia@gmail.com>
2019-10-08 13:04:49 +0800

    add, delete functions for todo app done

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import TodoItem
 # Create your views here.
 def todoView(request):
-    return HttpResponse('hello, todo view')
+    all_items = TodoItem.objects.all()
+    return render(request, 'todo.html',
+        {'list' : all_items} )
+
+def addTodo(request):
+    new_item=TodoItem(content = request.POST['content'])
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+def deleteTodo(request, todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect('/todo/')
